b77bc621dfa30d84916e226fa6e00721c7e291da
Hal Deadman <hal.deadman@gmail.com>
2019-05-05 19:12:22 -0400

    add test of bad algorithms with warning message if one is configured

--- core/cas-server-core-configuration/src/test/java/org/apereo/cas/configuration/support/CasConfigurationJasyptCipherExecutorTests.java
+++ core/cas-server-core-configuration/src/test/java/org/apereo/cas/configuration/support/CasConfigurationJasyptCipherExecutorTests.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.ArrayUtils;
 import org.jasypt.registry.AlgorithmRegistry;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
